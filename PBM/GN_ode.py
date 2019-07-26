ca8fd7d7082741dcb78a92d9cdecf4f2f67e5631
Soogwan Lee <lsg6140@gmail.com>
2019-07-26 17:53:48 +0900

    optimal step size, unsolved...

--- PBM/GN_ode.py
+++ PBM/GN_ode.py
@@ -42,16 +42,13 @@ def phi_z(func,z,k,n,p):
     phiz = np.empty(n*(p+1))
     dfdy = dfdy_ode(func,y,k,n)
     dfdk = dfdk_ode(func,y,k,n,p)
-    dJdt = np.matmul(dfdy,J)+dfdk
+    dJdt = dfdy@J+dfdk
     phiz[0:n] = func(y,k)
     phiz[n:] = dJdt.transpose().flatten()
     return phiz
 
 def state_jacob_int(func,y0,k,time):
-    if np.isscalar(y0):
-        n = 1
-    else:
-        n = np.size(y0)
+    n = np.size(y0)
     p = np.size(k)
     N = np.size(time)
     # initial condition J0 = 0
@@ -60,43 +57,39 @@ def state_jacob_int(func,y0,k,time):
     def dzdt(t,z):
         return phi_z(func,z,k,n,p)
     solution = solve_ivp(dzdt,[time[0],time[-1]],z0,method='Radau',t_eval=time)
+    if solution.success == False:
+        raise OverflowError("Integration by state_jacob_int failed")
     Z = solution.y
     Y = Z[0:n]
     J = Z[n:]
     Jt_i = np.hsplit(J,N)
     for i in range(N):
         Jt_i[i] = Jt_i[i].reshape(p,n).transpose()
-    return Y,Jt_i
+    return Y,Jt_i,solution.success
 
 def state_only_int(func,y0,k,time):
-    if np.isscalar(y0):
-        def dydt(y,t):
-            return func(y,k)
-        solution = odeint(dydt,y0,time)
-        return solution.transpose()
-    else:
-        def dydt(t,y):
-            return func(y,k)
-        solution = solve_ivp(dydt,[time[0],time[-1]],y0,method='Radau',t_eval=time)
-        return solution.y
-    return
+    def dydt(t,y):
+        return func(y,k)
+    solution = solve_ivp(dydt,[time[0],time[-1]],y0,method='Radau',t_eval=time)
+    return solution.y,solution.success
 
 def delta_k(J,Q,yhat,Y,p,N):
+    if np.shape(yhat) != np.shape(Y):
+        raise ValueError('size mismatch of yhat and Y')
     Hessian = np.zeros((p,p))
     gradient = np.zeros(p)
-    # Check whether y is scalar
-    if np.size(Q) == 1:
-        print('solve for scalar y')
-        for i in range(N):
-            Hessian += np.matmul(J[i].transpose(),J[i])
-            gradient += np.dot(J[i].transpose(),yhat[i]-Y[:,i])
-    else:
-        print('solve for vector y')
-        for i in range(N):
-            JQ = np.matmul(J[i].transpose(),Q)
-            Hessian += np.matmul(JQ,J[i])
-            gradient += np.dot(JQ,yhat[:,i]-Y[:,i])
-    del_k = np.linalg.solve(Hessian,gradient)
+    for i in range(N):
+        JQ = J[i].T@Q
+        Hessian += JQ@J[i]
+        gradient += JQ@(yhat[:,i]-Y[:,i])
+    # solve using singluar value decomposition
+    def svdsolve(a,b):
+        u,s,v = np.linalg.svd(a)
+        c = u.T@b
+        w = np.linalg.solve(np.diag(s),c)
+        x = v.T@w
+        return x
+    del_k = svdsolve(Hessian,gradient)
     return del_k
 
 def chi_squared(yhat,Y,Q,N):
@@ -106,27 +99,55 @@ def chi_squared(yhat,Y,Q,N):
         S = np.sum(diff**2)
     else:
         for i in range(N):
-            S += np.dot(np.matmul(diff[:,i],Q),diff[:,i])
+            # S += np.dot(np.matmul(diff[:,i],Q),diff[:,i])
+            S += diff[:,i]@Q@diff[:,i]
     return S
 
 def bisect(func,yhat,Q,k,time,iter_max):
     # check whether y is 1-dimensional
-    if np.size(yhat) == np.size(yhat,0):
-        y0 = yhat[0]
-        N = np.size(yhat)
-    else:
-        y0 = yhat[:,0]
-        N = np.size(yhat,1)
-    p = np.size(k)
-    Y,J = state_jacob_int(func,y0,k,time)
-    dk = delta_k(J,Q,yhat,Y,p,N)
-    mu = 1.0
-    S0 = chi_squared(yhat,Y,Q,N)
-    for j in range(iter_max):
-        k_temp = k + mu * dk
-        Y_temp = state_only_int(func,y0,k_temp,time)
-        S = chi_squared(yhat,Y_temp,Q,N)
-        if S < S0:
-            break
-        mu /= 2
-    return k_temp,Y,J
+    try:
+        if np.size(yhat) == np.size(yhat,0):
+            y0 = yhat[0]
+            N = np.size(yhat)
+        else:
+            y0 = yhat[:,0]
+            N = np.size(yhat,1)
+        p = np.size(k)
+        Y,J,suc = state_jacob_int(func,y0,k,time)
+        dk = delta_k(J,Q,yhat,Y,p,N)
+        mu = 1.0
+        S0 = chi_squared(yhat,Y,Q,N)
+        for j in range(iter_max):
+            k_next = k + mu * dk
+            Y_next,fos = state_only_int(func,y0,k_next,time)
+            if fos == False:
+                mu /= 2
+            else:
+                S = chi_squared(yhat,Y_next,Q,N)
+                if S < S0:
+                    break
+                mu /= 2
+        return Y,Y_next,J,dk,mu
+    except OverflowError:
+        print("Problem with integration. Try with another parameter")
+        return
+
+def optimal_step_size(Y,Y_next,yhat,J,dk,mu_a,Q,n,N):
+    beta = np.zeros(4)
+    for i in range(N):  
+        # Jdk = np.matmul(J[i],dk)
+        Jdk = J[i]@dk
+        dy = yhat[:,i]-Y[:,i]
+        r = (Y_next[:,i]-Y[:,i]-mu_a*Jdk)/mu_a**2
+        # rQ = np.matmul(r,Q)
+        rQ = r@Q
+        # beta[0] += 2*np.matmul(rQ,r)
+        beta[0] += 2*rQ@r
+        # beta[1] += 3*np.matmul(rQ,Jdk)
+        beta[1] += 3*rQ@Jdk
+        # beta[2] += np.matmul(np.matmul(Jdk.transpose(),Q),Jdk)-2*np.matmul(rQ,dy)
+        beta[2] += Jdk.transpose()@Q@Jdk-2*rQ@dy
+        # beta[3] += -np.matmul(np.matmul(Jdk.transpose(),Q),dy)
+        beta[3] += -Jdk.transpose()@Q@dy
+    mu_opt = np.roots(beta)
+    return mu_opt
