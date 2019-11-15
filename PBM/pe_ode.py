6241f384392242a085b7092b72e5e562fd317c44
Soogwan Lee <lsg6140@gmail.com>
2019-11-15 17:55:26 +0900

    Sg_ode.py added

--- PBM/pe_ode.py
+++ PBM/pe_ode.py
@@ -2,11 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp, odeint
 
-def dfdy_ode(func,y,k,n):
+def Sg_ode(ode,yhat,Q,k,time):
+    # check whether y is vector or matrix
+    try:
+        if np.size(yhat) == np.size(yhat,0):
+            y0 = yhat[0]
+            N = np.size(yhat)
+        else:
+            y0 = yhat[:,0]
+            N = np.size(yhat,1)
+
+        p = np.size(k)
+        Y,J,info = state_jacob_int(ode,y0,k,time)
+        S,diff = objective_func(yhat,Y,Q,N)
+        # calculation of S' = g
+        g = np.zeros(p)
+        for i in range(N):
+            g -= J[i].T@Q@diff[:,i]
+        return S, g
+    except OverflowError:
+        print("Problem with integration. Try with another parameters")
+        return
+
+def interpolate(a,b,phia,phib,dphia,n):
+    d = b-a
+    c = (phib-phia-d*dphia)/d**2
+    if c>=5*n*np.eps*b:
+        alpha = a-dphia/(2*c)
+        d = 0.1*d
+        alpha = min(max(alpha,a+d),b-d)
+    else:
+        alpha = (a+b)/2
+    return alpha
+
+def checkfgH(func,y,k):
+    return func(y,k)
+
+def dfdy_ode(ode,y,k,n):
     h = 1e-8
     y = y.astype(np.float)
     if np.isscalar(y):
-        dfdy = (func(y+h,k)-func(y-h,k))/(2*h)
+        dfdy = (ode(y+h,k)-ode(y-h,k))/(2*h)
         return dfdy
     else:
         dfdy = np.empty((n,n))
@@ -15,15 +51,15 @@ def dfdy_ode(func,y,k,n):
             yl = y.copy()
             yr[i] += h
             yl[i] -= h
-            dfdy[i] = (func(yr,k)-func(yl,k))/(2*h)
+            dfdy[i] = (ode(yr,k)-ode(yl,k))/(2*h)
         return dfdy.transpose()
     return
 
-def dfdk_ode(func,y,k,n,p):
+def dfdk_ode(ode,y,k,n,p):
     h = 1e-8
     k = k.astype(np.float)
     if p == 1:
-        dfdk = (func(y,k+h)-func(y,k-h))/(2*h)
+        dfdk = (ode(y,k+h)-ode(y,k-h))/(2*h)
         return dfdk
     else:
         dfdk = np.empty((p,n))
@@ -32,22 +68,22 @@ def dfdk_ode(func,y,k,n,p):
             kl = k.copy()
             kr[i] += h
             kl[i] -= h
-            dfdk[i] = (func(y,kr)-func(y,kl))/(2*h)
+            dfdk[i] = (ode(y,kr)-ode(y,kl))/(2*h)
         return dfdk.transpose()
     return
 
-def phi_z(func,z,k,n,p):
+def phi_z(ode,z,k,n,p):
     y = z[0:n]
     J = z[n:].reshape((p,n)).transpose()
     phiz = np.empty(n*(p+1))
-    dfdy = dfdy_ode(func,y,k,n)
-    dfdk = dfdk_ode(func,y,k,n,p)
+    dfdy = dfdy_ode(ode,y,k,n)
+    dfdk = dfdk_ode(ode,y,k,n,p)
     dJdt = dfdy@J+dfdk
-    phiz[0:n] = func(y,k)
+    phiz[0:n] = ode(y,k)
     phiz[n:] = dJdt.transpose().flatten()
     return phiz
 
-def state_jacob_int(func,y0,k,time):
+def state_jacob_int(ode,y0,k,time):
     n = np.size(y0)
     p = np.size(k)
     N = np.size(time)
@@ -55,7 +91,7 @@ def state_jacob_int(func,y0,k,time):
     z0 = np.zeros(n*(p+1))
     z0[0:n] = y0
     def dzdt(t,z):
-        return phi_z(func,z,k,n,p)
+        return phi_z(ode,z,k,n,p)
     solution = solve_ivp(dzdt,[time[0],time[-1]],z0,method='Radau',t_eval=time)
     if solution.success == False:
         raise OverflowError("Integration by state_jacob_int failed")
@@ -67,9 +103,9 @@ def state_jacob_int(func,y0,k,time):
         Jt_i[i] = Jt_i[i].reshape(p,n).transpose()
     return Y,Jt_i,solution.success
 
-def state_only_int(func,y0,k,time):
+def state_only_int(ode,y0,k,time):
     def dydt(t,y):
-        return func(y,k)
+        return ode(y,k)
     solution = solve_ivp(dydt,[time[0],time[-1]],y0,method='Radau',t_eval=time)
     return solution.y,solution.success
 
@@ -92,7 +128,7 @@ def delta_k(J,Q,yhat,Y,p,N):
     del_k = svdsolve(Hessian,gradient)
     return del_k
 
-def chi_squared(yhat,Y,Q,N):
+def objective_func(yhat,Y,Q,N):
     S = 0
     diff = yhat-Y
     if np.size(Q) == 1:
@@ -101,9 +137,9 @@ def chi_squared(yhat,Y,Q,N):
         for i in range(N):
             # S += np.dot(np.matmul(diff[:,i],Q),diff[:,i])
             S += diff[:,i]@Q@diff[:,i]
-    return S
+    return S, diff
 
-def bisect(func,yhat,Q,k,time,iter_max):
+def bisect(ode,yhat,Q,k,time,iter_max):
     # check whether y is 1-dimensional
     try:
         if np.size(yhat) == np.size(yhat,0):
@@ -113,17 +149,17 @@ def bisect(func,yhat,Q,k,time,iter_max):
             y0 = yhat[:,0]
             N = np.size(yhat,1)
         p = np.size(k)
-        Y,J,suc = state_jacob_int(func,y0,k,time)
+        Y,J,suc = state_jacob_int(ode,y0,k,time)
         dk = delta_k(J,Q,yhat,Y,p,N)
         mu = 1.0
-        S0 = chi_squared(yhat,Y,Q,N)
+        S0 = objective_func(yhat,Y,Q,N)
         for j in range(iter_max):
             k_next = k + mu * dk
-            Y_next,fos = state_only_int(func,y0,k_next,time)
+            Y_next,fos = state_only_int(ode,y0,k_next,time)
             if fos == False:
                 mu /= 2
             else:
-                S = chi_squared(yhat,Y_next,Q,N)
+                S = objective_func(yhat,Y_next,Q,N)
                 if S < S0:
                     break
                 mu /= 2
@@ -131,17 +167,3 @@ def bisect(func,yhat,Q,k,time,iter_max):
     except OverflowError:
         print("Problem with integration. Try with another parameter")
         return
-
-def optimal_step_size(Y,Y_next,yhat,J,dk,mu_a,Q,n,N):
-    beta = np.zeros(4)
-    for i in range(N):  
-        Jdk = J[i] @ dk
-        dy = yhat[:,i]-Y[:,i]
-        r = (Y_next[:,i]-Y[:,i]-mu_a*Jdk)/mu_a**2
-        rQ = r @ Q
-        beta[0] += 2*rQ @ r
-        beta[1] += 3*rQ @ Jdk
-        beta[2] += Jdk.T @ Q @ Jdk-2*rQ @ dy
-        beta[3] += -Jdk.T @ Q @ dy
-    mu_opt = np.roots(beta)
-    return mu_opt
